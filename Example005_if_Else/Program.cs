761000a8f990aff4941d3467f02ad4a648019a99
UzenLe <Uzenkovlesha@mail.ru>
2023-07-28 11:23:58 +0300

    Exemple005 If Else

---
+++ Example005_if_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "снежана")
+{
+    Console.WriteLine("Это же любимая жонка!");
+}
+else
+{
+     Console.Write("Здарова, ");
+     Console.WriteLine(username);
+}
