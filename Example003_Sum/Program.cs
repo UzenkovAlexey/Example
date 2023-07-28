45d1cd9cc59225799b1fa4dda8c4b2bab3774aad
UzenLe <Uzenkovlesha@mail.ru>
2023-07-28 10:59:32 +0300

    Exanple003_Random_sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,8 @@
-﻿int numberA = 30;
-int numberB = 50;
-int numberC = 60;
+﻿int numberA = new Random().Next(1,10);//0,1,2,3,4,5...9
+Console.WriteLine(numberA);                                                                                              
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,10);
+Console.WriteLine(numberC);
 int result = numberA + numberB + numberC;
 Console.WriteLine(result);
