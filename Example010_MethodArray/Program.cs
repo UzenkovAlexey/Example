43871e19c194553b2b4a84643ea23ceb3930b520
UzenLe <Uzenkovlesha@mail.ru>
2023-07-30 17:34:39 +0300

    Examoles  10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {157, 220, 31, 494, 543, 698, 751, 841, 926, 170, 101, 152,};
+
+int n = array.Length;
+int find = 926;
+
+int index = 0; 
+
+while (index < n) 
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break
+    }
+      //inndex = index =1;
+      index++;
+}
